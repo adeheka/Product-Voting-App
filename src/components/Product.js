66a267dc57c72a03b4e27fa3d8c51d0d41ac82fc
Oluwaferanmi Heka <adedokunoluwaferanmi@gmail.com>
2020-10-22 03:11:51 +0100

    redeclared caret icon

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import Caret from '../caret-up.svg'
+import {ReactComponent as Caret} from '../caret-up.svg'
 function Product({ id, image, url, style, count, title, description, avatar, onVote }) {
   function handleUpVote() {
     onVote(id)
   }
   return (
     <div className="product-card">
-      <div className="product-image"><img src={image} style={style} alt=""/></div>      
+      <div className="product-image">
+        <img src={image} style={style} alt=""/>
+      </div>      
       <div className="product-deets">
         <div className="vote-count">
           <p>
           <span>
             <a href={url} onClick={handleUpVote}>
-              <img src={Caret} style={{ height: '50px', width: '50px', color: 'blue' }} alt="" />
+              <span className="caret"></span>
+              <Caret style={{ height: '50px', width: '50px', fill: 'lightBlue' }} />
             </a>
           </span>
           {count}
